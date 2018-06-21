a8d2360aaa6d078c0d7a1bced49d9e19c2778734
Fortifelx <fortifelx@gmail.com>
2018-06-21 20:10:00 +0300

    start CRUD

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        \SocialiteProviders\Manager\ServiceProvider::class,
+
     ],
 
     /*
--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    'instagram' => [
+        'client_id' => env('edc47ec7ae1447eab3131c2f07d7fc66'),         // Your Client ID
+        'client_secret' => env('3f52d7e544954f9d8eba7cf14d88f484 '), // Your Client Secret
+        'redirect' => env('https://goome.ru'),
+    ],
 
 ];
